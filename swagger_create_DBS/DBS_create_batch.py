3dfec5ce995d3381087d11ea13bb505a5b07e1da
Tunglg Dzhamban <dzha2na@gmail.com>
2023-06-25 13:39:59 +0300

    Add files via upload

---
+++ swagger_create_DBS/DBS_create_batch.py
@@ -0,0 +1,31 @@
+# create NEW DBS CN (Unitrade ETA) template
+# using the data from data_dbs.xlsx (seller_seq, phone, street_address, contact_name, postcode)
+
+import json
+import pandas as pd
+import requests
+
+# Read Excel file as a DataFrame
+df = pd.read_excel('data_dbs.xlsx')
+
+# Convert DataFrame to list of dictionaries
+records = df.to_dict('records')
+
+# Print the records row by row
+for record in records:
+    seq = record['seq']
+    xphone = record['xphone']
+    xstreet_address = record['xstreet_address']
+    xcontact_name = record['xcontact_name']
+    xpostcode = record['xpostcode']
+    a = json.dumps({"user_id": str(seq) , "seller_id": str(seq) , "parent_seller_id": str(seq) ,"havana_id":0,"session_id":"","intl_locale":"ru_RU","ip":""})
+
+    # Make the API call using the extracted information
+    url = "http://logistics-sx-web-onboarding.prod1.k8s.ae-rus.net/api/v1/onboarding/create-dbs-template"
+    headers = {"accept": "application/json", "x-aer-seller-info": a, "Content-Type": "application/json"}
+    data = json.dumps({"custom_shipping_promises": [{"destination_zone_id": 1,"shipping_fee": 0,"commit_day": 20},{"destination_zone_id": 2,"shipping_fee": 0,"commit_day": 20},{"destination_zone_id": 3,"shipping_fee": 0,"commit_day": 40},{"destination_zone_id": 4,"shipping_fee": 0,"commit_day": 40},{"destination_zone_id": 5,"shipping_fee": 0,"commit_day": 40}],"warehouse": {"name": "dbs wh","phone": str(xphone),"country_code": "CN","city_code": "900100010001000000","province_code": "900100010000000000","street_address": str(xstreet_address),"contact": str(xcontact_name),"entry_comment": "","post_code": str(xpostcode)}})
+
+    response = requests.post(url, headers=headers, data=data)
+
+    print(f"Response for row {record}: {response.status_code}")
+    print(response.json())
---
+++ swagger_create_DBS/DBS_create_one.py
@@ -0,0 +1,59 @@
+# Create NEW DBS template with manually filled data
+# Use if need to create one DBS template
+
+import json
+import requests
+
+url_swagger = "http://logistics-sx-web-onboarding.prod1.k8s.ae-rus.net/api/v1/onboarding/create-dbs-template"
+header = {
+    "accept": "application/json",
+    "x-aer-seller-info": '{"user_id": 4126555948, "seller_id":4126555948, "parent_seller_id":4126555948,"havana_id":0,"session_id":"","intl_locale":"ru_RU","ip":""}',
+    "Content-Type": "application/json"
+}
+data_raw = json.dumps(
+    {
+        "custom_shipping_promises": [
+            {
+                "destination_zone_id": 1,
+                "shipping_fee": 0,
+                "commit_day": 20
+            },
+            {
+                "destination_zone_id": 2,
+                "shipping_fee": 0,
+                "commit_day": 20
+            },
+            {
+                "destination_zone_id": 3,
+                "shipping_fee": 0,
+                "commit_day": 40
+            },
+            {
+                "destination_zone_id": 4,
+                "shipping_fee": 0,
+                "commit_day": 40
+            },
+            {
+                "destination_zone_id": 5,
+                "shipping_fee": 0,
+                "commit_day": 40
+            }
+        ],
+        "warehouse": {
+            "name": "Test dbs wh",
+            "phone": "909991112233",
+            "country_code": "TR",
+            "city_code": "921800340039000000",
+            "province_code": "900100020000000000",
+            "street_address": "Уральская ул дом 119/2",
+            "contact": "Ванко Иван Иванович",
+            "entry_comment": "",
+            "post_code": "10005"
+        }
+    }
+)
+
+response = requests.post(url_swagger, headers=header, data=data_raw)
+
+print(response.status_code)
+print(response.json())
