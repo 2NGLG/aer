763dc1eea842c60fba642af4b2c5d56fcd08f0af
Tunglg Dzhamban <dzha2na@gmail.com>
2023-06-25 13:52:12 +0300

    Delete create_sql_id_email.sql

--- SQL_sg/create_sql_id_email.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS temp_slr_email
-(
-    seller_admin_seq                   BIGINT COMMENT 'seller_admin_seq',
-    seller_admin_id                    STRING COMMENT 'seller_admin_id',
-    email                              STRING COMMENT 'login email'
-)
